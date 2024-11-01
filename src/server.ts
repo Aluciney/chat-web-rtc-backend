04991b9802aa3fa302eaad43a0048476355c8526
Aluciney de Souza Wanderley <aluciney.wanderley@amazonasenergia.com>
2024-11-01 13:16:14 -0400

    Multe usuarios

--- src/server.ts
+++ src/server.ts
@@ -5,47 +5,40 @@ import socket from 'socket.io';
 
 const PORT = process.env.PORT || 3001;
 
-let ROOMS = [
-	{ name: 'Sala 1', value: 'saadamkweg-weg-wvsdsdv-asdasfdhrjtm' },
-	{ name: 'Sala 2', value: 'dqwdqwdqw-fsdfsdfsdfgsg-asdasdasdas' }
-];
 
 const app = express();
 app.use(cors());
-app.get('/rooms', (req, res) => {
-	res.json(ROOMS);
-	return;
-});
+
 const server = http.createServer(app);
 const io = new socket.Server(server, { cors: { origin: '*' } });
 
 io.on('connection', (socket) => {
-	const { room } = socket.handshake.query;
-	if (!room) {
-		socket.disconnect();
-		return;
-	}
-	const ROOM = ROOMS.find(room_ => room_.value === room);
-	if(!ROOM){
-		socket.disconnect();
-		return;
-	}
-	socket.join(room);
-	socket.on('offer', ({ room: room_, offer }) => {
-		socket.to(room).emit('offer', offer);
+	socket.on('join-room', (roomId) => {
+		socket.join(roomId);
+
+		socket.to(roomId).emit('user-connected', { id: socket.id });
+
+		socket.on('users', () => {
+			const users = Array.from(io.sockets.adapter.rooms.get(roomId) || []).filter(id => id !== socket.id).map(id => ({ id }));
+			socket.emit('existing-users', users);
+		});
+
+		socket.on('offer', ({ userId, offer }) => {
+			socket.to(userId).emit('offer', { userId: socket.id, offer });
+		});
+
+		socket.on('answer', ({ userId, answer }) => {
+			socket.to(userId).emit('answer', { userId: socket.id, answer });
+		});
+
+		socket.on('candidate', ({ userId, candidate }) => {
+			socket.to(userId).emit('candidate', { userId: socket.id, candidate });
+		});
+
+		socket.on('disconnect', () => {
+			socket.to(roomId).emit('user-disconnected', { id: socket.id });
+		});
 	})
-
-	socket.on('answer', ({ room: room_, answer }) => {
-		socket.to(room).emit('answer', answer);
-	});
-
-	socket.on('candidate', ({ room: room_, candidate }) => {
-		socket.to(room).emit('candidate', candidate);
-	});
-
-	socket.on('disconnect', (socket) => {
-
-	});
 });
 
 server.listen(3001, () => console.log(`Starting server on port ${PORT}`));
